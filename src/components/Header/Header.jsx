54edd98e61d3826f6a72111b04a41bd304991e89
pablodev <pabloteixeira591@gmail.com>
2024-12-03 22:38:14 -0300

    Atualização do header.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 export default function Header() {
     return (
         <div className="header">
-            <img className="imgLogo" src="/athon-logo.jpg" alt="" />
+            <img className="imgLogo" src="./athon-logo.jpg" alt="" />
             <h1 className="textLogo">Quem sou eu?</h1>
         </div>
     )
