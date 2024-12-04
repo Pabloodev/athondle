b641f2c7cec20c4b48a06d99d7fd2d7903976cad
pablodev <pabloteixeira591@gmail.com>
2024-12-03 22:50:47 -0300

    Atualização do projeto

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import './Header.css'
+import photo from  '../../assets/athon-logo.jpg'
+import { CloudSnow } from 'lucide-react'
 
 export default function Header() {
     return (
         <div className="header">
-            <img className="imgLogo" src="./athon-logo.jpg" alt="" />
+            <img className="imgLogo" src={photo} alt="" />
             <h1 className="textLogo">Quem sou eu?</h1>
         </div>
     )
