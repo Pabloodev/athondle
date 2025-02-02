ed3b3bd8208b76dc82d6e94af9b04461d3608794
pablodev <pabloteixeira591@gmail.com>
2025-02-01 23:13:13 -0300

    update database and add animation on popup message

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,25 +1,32 @@
 import { useState } from "react";
-import styles from './Popup.module.css'
+import styles from "./Popup.module.css";
 import { useEffect } from "react";
+
+import { motion } from "motion/react";
 
 const Popup = () => {
   const [popupOn, setPopUpOn] = useState(false);
 
   useEffect(() => {
-    setPopUpOn(true)
+    setPopUpOn(true);
     setTimeout(() => {
-      setPopUpOn(false)
+      setPopUpOn(false);
     }, 4500);
-
-  },[])
-
-  
+  }, []);
 
   return (
     popupOn && (
-      <div className={styles.popup}>
-        <p>Este jogo está sempre sendo atualizado para melhorar a experiência do jogador. Seu feedback é valioso para nós!</p>
-      </div>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ delay: 1, duration: 0.5, ease: "easeInOut" }}
+        className={styles.popup}
+      >
+        <p>
+          Este jogo está sempre sendo atualizado para melhorar a experiência do
+          jogador. Seu feedback é valioso para nós!
+        </p>
+      </motion.div>
     )
   );
 };
